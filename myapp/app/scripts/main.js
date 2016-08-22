a1a102284f2434bd670289f2183d1c8608f0d3c6
Prithvi <prithvi.anantharaj@gmail.com>
2016-08-22 12:20:37 -0700

    Displaying Lat/Long of a city using Google Maps API.

--- myapp/app/scripts/main.js
+++ myapp/app/scripts/main.js
@@ -1,6 +1,30 @@
-console.log('\'Allo \'Allo!');
-
 $(document).ready(function() {
-	$('.collapse').collapse();
-	
+
+    $('#submit_city').click(function() {
+        var $city = $('#search_city').val(); //city, state
+
+        $.ajax({
+            url: 'http://maps.google.com/maps/api/geocode/json?address=' + $city,
+            dataType: 'json',
+            type: 'get',
+            cache : true,
+            success: function(response) {
+               $(response.results).each(function(index, value) {
+                    var lat = value.geometry.location.lat;
+                    var lng =  value.geometry.location.lng;
+                    $('#lat').empty().append(lat);
+                    $('#lng').empty().append(lng);
+                    var map_url = 'https://maps.googleapis.com/maps/api/staticmap?center='+ $city + '&size=300x300';
+                    $('.map_display').append('<img src= "'+ map_url+ '"/>');
+               });
+            },
+            error: function(){
+                alert("Did not work!");
+            }
+        });
+    });
+
+
+
+    
 });
