3e39905c1a1804fd903118667d594678b0749015
Prithvi <prithvi.anantharaj@gmail.com>
2016-08-22 12:27:05 -0700

    clean up

--- myapp/app/scripts/main.js
+++ myapp/app/scripts/main.js
@@ -10,11 +10,16 @@ $(document).ready(function() {
             cache : true,
             success: function(response) {
                $(response.results).each(function(index, value) {
+                    // Fetch the latitude of the city
                     var lat = value.geometry.location.lat;
+                    // Fetch teh longitude of the city
                     var lng =  value.geometry.location.lng;
+                    // Insert the latitude/longitude into the DOM
                     $('#lat').empty().append(lat);
                     $('#lng').empty().append(lng);
+                    // Fetch the map image url of city
                     var map_url = 'https://maps.googleapis.com/maps/api/staticmap?center='+ $city + '&size=300x300';
+                    // Insert the image on the HTML
                     $('.map_display').append('<img src= "'+ map_url+ '"/>');
                });
             },
@@ -23,8 +28,5 @@ $(document).ready(function() {
             }
         });
     });
-
-
-
     
 });
